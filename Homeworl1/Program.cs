d89276f0097f669394754cde0974704722e88c9d
valek90@bk.ru <valek90@bk.ru>
2023-02-13 14:03:40 +0300

    add Homework1

---
+++ Homeworl1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+int a = Convert. ToInt32(Console.ReadLine());
+int b = Convert. ToInt32(Console. ReadLine());
+if(a > b)
+{
+    Console.WriteLine("max = " + a.ToString());
+}
+else if (b > a)
+{
+    Console.WriteLine("max = " + b.ToString());
+}
